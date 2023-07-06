b36d09746fc6cd94f91c962cb9a3353212dcadf1
Arthur Anderson <>
2023-07-06 10:14:51 -0600

    add packet sending

--- src/comm.c
+++ src/comm.c
@@ -1,15 +1,16 @@
 #include "comm.h"
 #include "graph.h"
-#include <sys/socket.h>
-#include <sys/select.h>
-#include <pthread.h>
-#include <netinet/in.h>
+#include <errno.h>
 #include <memory.h>
+#include <netdb.h> /*for struct hostent*/
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <netdb.h> /*for struct hostent*/
-#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <unistd.h>
 
 static uint16_t udp_port_number = 40000;
 uint16_t get_next_udp_port_number() {
@@ -33,23 +34,31 @@ init_udp_socket(node_t * node) {
 }
 
 static char recv_buffer[MAX_PACKET_BUFFER_SIZE];
+static char send_buffer[MAX_PACKET_BUFFER_SIZE];
+
+int
+pkt_receive(node_t *node, interface_t *interface, 
+            char *pkt, unsigned int pkt_size) {
+    printf("msg recvd: %s, on node: %s, IIF = %s\n", pkt, node->node_name, interface->if_name);
+    return 0;
+}
 
 static void
 _pkt_receive(node_t *receving_node, 
             char *pkt_with_aux_data, 
             unsigned int pkt_size){
 
-    // char *recv_intf_name = pkt_with_aux_data;
-    // interface_t *recv_intf = get_node_if_by_name(receving_node, recv_intf_name);
+    char *recv_intf_name = pkt_with_aux_data;
+    interface_t *recv_intf = get_node_if_by_name(receving_node, recv_intf_name);
 
-    // if(!recv_intf){
-    //     printf("Error : Pkt recvd on unknown interface %s on node %s\n", 
-    //                 recv_intf->if_name, receving_node->node_name);
-    //     return;
-    // }
+    if(!recv_intf){
+        printf("Error : Pkt recvd on unknown interface %s on node %s\n", 
+                    recv_intf->if_name, receving_node->node_name);
+        return;
+    }
 
-    // pkt_receive(receving_node, recv_intf, pkt_with_aux_data + IF_NAME_SIZE, 
-    //             pkt_size - IF_NAME_SIZE);
+    pkt_receive(receving_node, recv_intf, pkt_with_aux_data + IF_NAME_SIZE, 
+                pkt_size - IF_NAME_SIZE);
 }
 
 static void *
@@ -93,6 +102,7 @@ _network_start_pkt_receiver_thread(void *arg){
 
         memcpy(&active_sock_fd_set, &backup_sock_fd_set, sizeof(fd_set));
 
+        // Watch all the socket file descriptors in this network
         select(sock_max_fd + 1, &active_sock_fd_set, NULL, NULL, NULL);
 
         ITERATE_GLTHREAD_BEGIN(&topo->node_list, curr){
@@ -124,4 +134,51 @@ network_start_pkt_receiver_thread(graph_t *topo) {
     pthread_create(&recv_pkt_thread, &attr, 
                     _network_start_pkt_receiver_thread, 
                     (void *)topo);
+}
+
+static int
+_send_pkt_out(int sock_fd, char * pkt_data, uint32_t pkt_size, uint16_t dst_udp_port) {
+    int rc;
+    struct sockaddr_in dest_addr;
+
+    struct hostent * host = (struct hostent *)gethostbyname("127.0.0.1");
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = dst_udp_port;
+    dest_addr.sin_addr = *((struct in_addr *)host->h_addr);
+
+    rc = sendto(sock_fd, pkt_data, pkt_size, 0, 
+        (struct sockaddr *)&dest_addr, sizeof(struct sockaddr));
+    return rc;
+}
+
+int
+send_pkt_out(char *pkt, unsigned int pkt_size, interface_t *interface) {
+    int32_t rc = 0;
+    node_t * sending_node = interface->att_node;
+    node_t * nbr_node = get_nbr_node(interface);
+
+    if (!nbr_node) 
+        return -1;
+
+    uint16_t dst_udp_port = nbr_node->udp_port_number;
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (sock < 0) {
+        printf("Error: sending socket creation failed errno=%d\n", errno);
+        return -1;
+    }
+
+    interface_t * other_interface = &interface->link->if1 == interface ? \
+                                    &interface->link->if2 : &interface->link->if1;
+
+    memset(send_buffer, 0, MAX_PACKET_BUFFER_SIZE);
+
+    char * pkt_with_aux_data = send_buffer;
+    strncpy(pkt_with_aux_data, other_interface->if_name, IF_NAME_SIZE);
+    pkt_with_aux_data[IF_NAME_SIZE] = '\0';
+    memcpy(pkt_with_aux_data + IF_NAME_SIZE, pkt, pkt_size);
+
+    rc = _send_pkt_out(sock, pkt_with_aux_data, pkt_size + IF_NAME_SIZE, dst_udp_port);
+    close(sock);
+    return rc;    
 }
--- src/maincli.c
+++ src/maincli.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include "CommandParser/libcli.h"
 #include "graph.h"
+#include "comm.h"
 
 extern void nw_init_cli();
 extern graph_t * build_first_topo();
@@ -10,6 +12,15 @@ main(int argc, char **argv) {
 
 	nw_init_cli();
 	topo = build_first_topo();
+
+
+	sleep(2); // give some time to receive the data
+
+	node_t * snode = get_node_by_node_name(topo, "R0_re");
+	interface_t * oif = get_node_if_by_name(snode, "eth0/0");
+	char msg[] = "Hello, how are you\0";
+	send_pkt_out(msg, strlen(msg), oif);
+
 	start_shell();
 
 	return 0;
